b2d41d7b542617485604b8e3813e4ada47f1ff67
Sergey Lebedev <sergejlebedev31838@gmail.com>
2024-07-20 17:00:12 +0700

    Add cycle.c

---
+++ cycle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    return 0;
+}
+
+// Циклы
+int cycle() {
+    for (int i = 0; i < 5; i++) {
+        printf("Iteration %d\n", i);
+    }
+
+    int j = 0;
+    while (j < 5) {
+        printf("While iteration %d\n", j);
+        j++;
+    }    
+}
