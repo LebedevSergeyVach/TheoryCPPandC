b4b1e52130f5a284ccaef5bc602c41ebe397bf1a
Sergey Lebedev <sergejlebedev31838@gmail.com>
2024-07-27 18:02:59 +0700

    Update cycle.c

--- cycle.c
+++ cycle.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+// Функция, возращающее перевернутое число 103020 -> 20301
+int revesedNumber(int number);
+
 /* Циклы for, while и do while */
 int main()
 {
@@ -17,12 +20,32 @@ int main()
         j++;
     }
 
-    // [i, 5]
     int i = 0;
+    // [i, 5]
     do {
         printf("Iteration %d\n", i);
         i++;
     } while (i <= 5);
 
+    int number, reversed;
+    scanf("%d", &number);
+
+    reversed = revesedNumber(number);
+    printf("Reversed number -> %d\n", reversed);
+
     return 0;
 }
+
+// Функция, возращающее перевернутое число 103020 -> 20301
+int revesedNumber(int number)
+{
+    int reversed = 0;
+
+    while (number > 0) {
+        reversed = reversed * 10 + number % 10;
+        number /= 10;
+    }
+
+    return reversed;
+}
+
