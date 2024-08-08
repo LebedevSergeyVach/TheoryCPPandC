5cbed4d38b6e7e93e99f4ab3697a725406753929
Sergey Lebedev <sergejlebedev31838@gmail.com>
2024-08-08 15:46:14 +0700

    Update cycle.c

--- cycle.c
+++ cycle.c
@@ -3,7 +3,7 @@
 // Функция, возращающее перевернутое число 103020 -> 20301
 int revesedNumber(int number);
 
-/* Циклы for, while и do while */
+/* Циклы for, while, do while и goto */
 int main()
 {
     // [i, 5]
@@ -33,17 +33,43 @@ int main()
         Напишем вычисление факториала n! через цикл for
         Начиная со стандарта С99 можно объявлять переменную i прямо в операторе for. По принципу "где нужно, там и объявили".
     */
-    for (res = 1, i = 1; i <= n; res *= i, i++) {
-        ;       // подчеркиваем, что мы осознаем, что в цикле тело пустое
+    for (res = 1, i = 1; i <= n; res *= i, i++)
+    {
+        ; // подчеркиваем, что мы осознаем, что в цикле тело пустое
     }
     printf("%d! = %d\n", n, res);
-
 
     int number, reversed;
     scanf("%d", &number);
 
     reversed = revesedNumber(number);
     printf("Reversed number -> %d\n", reversed);
+
+    /*
+        goto cycle
+
+        Когда использовать goto:
+        Использование goto понижает читаемость кода. 
+        Бесконечный цикл лучше написать как while(1) или for(;;).
+
+        goto стоит использовать только в 3 случаях:
+        - break по внешнему циклу.
+        - continue по внешнему циклу.
+        - обработка ошибок (об этом будет подробно рассказано в курсе про IPC).
+    */
+    int i = 0;
+
+// Метка start: для goto
+start:
+    // [i; 4]
+    if (i < 5)
+    {
+        printf("i = %d\n", i);
+        i++;
+        goto start;
+    }
+
+    printf("End cycle goto\n");
 
     return 0;
 }
