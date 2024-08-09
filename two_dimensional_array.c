e5fad1e8948214dd9e15a540aa0e49a7e060011d
Sergey Lebedev <sergejlebedev31838@gmail.com>
2024-08-09 16:05:42 +0700

    Update two_dimensional_array.c

--- two_dimensional_array.c
+++ two_dimensional_array.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
-// Для чтения данных из .txt файла -> gcc main.c && ./a < assets/data.txt
+// Для чтения данных из .txt файла -> gcc two_dimensional_array.c && ./a < assets/data.txt && rm a
 
 /*
-    Двухмерный массив
+    Двухмерный массив или матрица
 */
 int main()
 {
-    int row, column;
-    printf("Enter ROW and COLUMN for two-dimensional array (row column): ");
-    scanf("%d%d", &row, &column);
+    int rows, columns, sum = 0;
+    printf("Enter ROW and COLUMN for two-dimensional array (rows columns): ");
+    scanf("%d%d", &rows, &columns);
 
     // Создание двухмерного массива
-    int a[row][column];
+    int a[rows][columns];
 
     // Инициализация данными двухмерного массива
     printf("\nEnter two-dimensional array:\n");
-    for (int i = 0; i < row; i++)
+    for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < column; j++)
+        for (int j = 0; j < columns; j++)
         {
             scanf("%d", &a[i][j]);
         }
@@ -25,14 +25,25 @@ int main()
 
     // Выведение в консоль двухмерного массива
     printf("\nTwo-dimensional array:\n");
-    for (int i = 0; i < row; i++)
+    for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < column; j++)
+        for (int j = 0; j < columns; j++)
         {
             printf("%3d, ", a[i][j]);
         }
         printf("\n");
     }
+
+    // Посчитаем сумму всех значений внутри матрицы
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            sum += a[i][j];
+        }
+    }
+
+    printf("\nThe sum of all the values of the matrix = %d\n", sum);
 
     return 0;
 }
