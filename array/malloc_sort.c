9fb4fe3878c46b55b80b867dc08d0a30a6b6654e
Sergey Lebedev <sergejlebedev31838@gmail.com>
2024-08-28 21:48:12 +0700

    Main: Add array/malloc_sort.c

---
+++ array/malloc_sort.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int input_array(int *array, int number);
+void output_array(const int *array, int number);
+void sotr_array(int *array, int number);
+void swap(int *a, int *b);
+
+int main() {
+    int programm = 0;
+
+    int number;
+    if (scanf("%d", &number) != 1 || getchar() != '\n') {
+        programm = 1;
+        printf("n/a");
+    } else {
+        int *array = (int *)malloc(number * sizeof(int));
+
+        if (array == NULL) {
+            programm = 1;
+            printf("n/a");
+        } else {
+            int input = input_array(array, number);
+
+            if (input != 0) {
+                programm = 1;
+                printf("n/a");
+            } else {
+                sotr_array(array, number);
+                output_array(array, number);
+                free(array);
+            }
+        }
+    }
+
+    return programm;
+}
+
+int input_array(int *array, int numbe) {
+    int programm = 0;
+
+    for (int *p = array; p - array < numbe; p++) {
+        scanf("%d", p);
+    }
+
+    if (getchar() != '\n') {
+        programm = 1;
+    }
+
+    return programm;
+}
+
+void output_array(const int *array, int number) {
+    for (int i = 0; i < number; i++) {
+        printf("%d ", array[i]);
+    }
+}
+
+// Selection Sort - Сортировка по выбору
+void sotr_array(int *array, int number) {
+    for (int i = 0; i < number; i++) {
+        int min_element = i;
+        for (int j = i + 1; j < number; j++)
+            if (array[j] < array[min_element]) min_element = j;
+        swap(&array[min_element], &array[i]);
+    }
+}
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
