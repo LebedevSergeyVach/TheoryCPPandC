c0e5b5352203a5e0f93fd72c4ef57dd6ef597081
Lebedev Sergey <sergejlebedev31838@gmail.com>
2025-02-27 01:11:13 +0700

    Main: Add tempArray/

---
+++ tempArray/colorOut.h
@@ -0,0 +1,11 @@
+#ifndef COLOR_OUT_H
+#define COLOR_OUT_H
+
+#define VIOLET "\033[35m"
+#define BLUE "\033[34m"
+#define YELLOW "\033[33m"
+#define GREEN "\033[32m"
+#define RED "\033[31m"
+#define RESET "\033[0m"
+
+#endif
